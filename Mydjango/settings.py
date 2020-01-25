d8a5db72d6557c42fa82d2af9d312e0ee08e41da
sushil <aidmankita1@gmail.com>
2020-01-25 18:49:58 +0530

    setup proper functionality here

--- Mydjango/settings.py
+++ Mydjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# Notes for me<<<<<<<<<<<<<<<-----------------------------------------
+# here each class in this page is actually a models in Django framwork
+
+# Question class will hold all questin and its data
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def __str__(self):
+        return self.question_text
+
+# choice class will handel the question option
+class Choice(models.Model):
+        question = models.ForeignKey(Question,on_delete=models.CASCADE)
+        choice_text = models.CharField(max_length=200)
+        votes = models.IntegerField(default=0)
+
+        def __str__(self):
+            return self.choice_text
