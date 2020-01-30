0238e412a9c8e4bf131cf573f9548b53c421d0bc
heysushil <sushil.chaudhary111@gmail.com>
2020-01-30 18:35:50 +0530

    changes on views behalf of tests

--- polls/views.py
+++ polls/views.py
@@ -22,7 +22,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """ return the latest five question """
+        """ Return the last five published questions (not including those set to be
+    published in the future). """
         return Question.objects.order_by('-pub_date')[:5]
     # fetch qustion list order by date limit 5 by this line
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
